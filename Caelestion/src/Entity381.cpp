07dfad3d253a814a4f00001b0c7474260f0c9b65
JSiao <jecsiao@gmail.com>
2018-05-14 05:57:56 +0000

    Final Touches

--- Caelestion/src/Entity381.cpp
+++ Caelestion/src/Entity381.cpp
@@ -360,7 +360,7 @@ caelestionStation::caelestionStation(Engine *engine, Ogre::Vector3 pos, int iden
 	Ogre::Degree z = Ogre::Degree(180.);
 	this->actualFacing.FromAngleAxis(z, Ogre::Vector3::UNIT_Y);
 
-	this->currentHealth = this->maxHealth = 330000;
+	this->currentHealth = this->maxHealth = 66000;
 
 	this->speed = 0.f;
 	this->turnRate = 0.f;
@@ -490,7 +490,7 @@ Yggdrasil::Yggdrasil(Engine *engine, Ogre::Vector3 pos, int identity):
 	Ogre::Degree z = Ogre::Degree(180.);
 	this->actualFacing.FromAngleAxis(z, Ogre::Vector3::UNIT_Y);
 
-	this->currentHealth = this->maxHealth = 250000;
+	this->currentHealth = this->maxHealth = 50000;
 
 	this->speed = 0.f;
 	this->turnRate = 0.f;
